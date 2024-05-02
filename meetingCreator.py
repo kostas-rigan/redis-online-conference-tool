d2cb6c3f39d0ffbf4b83a9eaf601e2221fd5499b
NikosNikolaidis02 <t8200120@aueb.gr>
2024-05-02 12:50:57 +0300

    Updating meeting creator

--- meetingCreator.py
+++ meetingCreator.py
@@ -2,23 +2,24 @@ import json
 import random
 import string
 
-def generate_meeting():
+def generate_meeting(users):
     meeting_id = ''.join(random.choices(string.ascii_letters + string.digits, k=8))
     title = ''.join(random.choices(string.ascii_uppercase, k=6))
     description = ''.join(random.choices(string.ascii_uppercase, k=6))
     description = 'This meeting is about ' + description
-    isPublic = random.randint(1, 2)
-    #audience
+    isPublic = random.randint(0, 1)
+    audience = random.sample(users, k=random.randint(1, len(users)))  # Randomly select emails from users list
+    audience_emails = [user['email'] for user in audience]  # Extract emails
     return {
         'meeting_id': meeting_id,
         'title': title,
         'description': description,
-        'isPublic': isPublic,
-        #'audience': audience
+        'isPublic': bool(isPublic),
+        'audience': audience_emails
     }
 
-def generate_meetings(num_users):
-    meetings = [generate_meeting() for _ in range(num_users)]
+def generate_meetings(users, num_meetings):
+    meetings = [generate_meeting(users) for _ in range(num_meetings)]
     return meetings
 
 def write_to_json(meetings, filename):
@@ -26,6 +27,9 @@ def write_to_json(meetings, filename):
         json.dump(meetings, f, indent=4)
 
 if __name__ == "__main__":
-    num_meetings = 4  # Change this number to generate different number of users
-    meetings = generate_meetings(num_meetings)
+    with open('users.json') as f:
+        users = json.load(f)
+    
+    num_meetings = 20  # Change this number to generate different number of meetings
+    meetings = generate_meetings(users, num_meetings)
     write_to_json(meetings, 'meetings.json')
