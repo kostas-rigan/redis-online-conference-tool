21d934b3f53902d7371a2ff065b754d6dee6e692
kostas-rigan <t8200145@aueb.gr>
2024-05-07 22:12:01 +0300

    insert data into redis

--- main.py
+++ main.py
@@ -20,23 +20,32 @@ with open('meetings.json') as f:
 with open('meetings_instances.json') as f:
     meetings_instances = json.load(f)
 
-# Store users
-for user in users:
-    r.hset('users', user['userID'], json.dumps(user))
+with r.pipeline() as pipe:
+    # Store users
+    for pos, user in enumerate(users):
+        for key, value in user.items():
+            pipe.hset(f'users_{pos}', key, value)
 
-# Store events_log
-for event in events_log:
-    r.hset('events_log', event['event_id'], json.dumps(event))
+    # Store events_log
+    for pos, event in enumerate(events_log):
+        for key, value in event.items():
+            pipe.hset(f'events_log_{pos}', key, value)
 
-# Store meetings
-for meeting in meetings:
-    r.hset('meetings', meeting['meetingID'], json.dumps(meeting))
+    # Store meetings
+    for pos, meeting in enumerate(meetings):
+        for key, value in meeting.items():
+            if key == 'audience':
+                value = '|'.join(value)
+            pipe.hset(f'meetings_{pos}', key, value)
 
-# Store meetings_instances
-for meeting_instance in meetings_instances:
-    r.hset('meetings_instances', meeting_instance['meeting_id'], json.dumps(meeting_instance))
+    # Store meetings_instances
+    for pos, meeting_instance in enumerate(meetings_instances):
+        for key, value in meeting_instance.items():
+            pipe.hset(f'meetings_instances_{pos}', key, value)
 
-# Optionally, you can also create indexes for faster retrieval
-# For example, if you frequently search users by email, you can create an index like this:
-for user in users:
-    r.set('email_index:' + user['email'], user['userID'])
+    # Optionally, you can also create indexes for faster retrieval
+    # For example, if you frequently search users by email, you can create an index like this:
+    for user in users:
+        pipe.set('email_index:' + user['email'], user['userID'])
+
+    pipe.execute()
