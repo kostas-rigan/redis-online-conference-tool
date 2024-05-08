0a04033a7270539018a020fc4c7b886f7bb7a72c
kostas-rigan <t8200145@aueb.gr>
2024-05-08 09:23:39 +0300

    refactored main file

--- main.py
+++ main.py
@@ -1,51 +1,42 @@
 import json
 import redis
 
-# Connect to Redis
-r = redis.Redis(host='localhost', port=6379, db=0)
 
-# Load users.json
-with open('users.json') as f:
-    users = json.load(f)
+def load_json(filename):
+    with open(filename) as f:
+        obj = json.load(f)
+    return obj
 
-# Load events_log.json
-with open('events_log.json') as f:
-    events_log = json.load(f)
 
-# Load meetings.json
-with open('meetings.json') as f:
-    meetings = json.load(f)
-
-# Load meetings_instances.json
-with open('meetings_instances.json') as f:
-    meetings_instances = json.load(f)
-
-with r.pipeline() as pipe:
-    # Store users
-    for pos, user in enumerate(users):
-        for key, value in user.items():
-            pipe.hset(f'users_{pos}', key, value)
-
-    # Store events_log
-    for pos, event in enumerate(events_log):
-        for key, value in event.items():
-            pipe.hset(f'events_log_{pos}', key, value)
-
-    # Store meetings
-    for pos, meeting in enumerate(meetings):
-        for key, value in meeting.items():
-            if key == 'audience':
+def insert_to_redis(pipe, entity_pl, entity_name):
+    for pos, entity in enumerate(entity_pl):
+        for key, value in entity.items():
+            if isinstance(value, list):
                 value = '|'.join(value)
-            pipe.hset(f'meetings_{pos}', key, value)
+            pipe.hset(f'{entity_name}_{pos}', key, value)
 
-    # Store meetings_instances
-    for pos, meeting_instance in enumerate(meetings_instances):
-        for key, value in meeting_instance.items():
-            pipe.hset(f'meetings_instances_{pos}', key, value)
 
-    # Optionally, you can also create indexes for faster retrieval
-    # For example, if you frequently search users by email, you can create an index like this:
-    for user in users:
-        pipe.set('email_index:' + user['email'], user['userID'])
+def main():
+    # Connect to Redis
+    r = redis.Redis(host='localhost', port=6379, db=0)
 
-    pipe.execute()
+    files = ['users.json', 'events_log.json', 'meetings.json', 'meetings_instances.json']
+    content = {}
+    for file in files:
+        entity = file.split('.')[0]
+        content[entity] = load_json(file)
+
+    with r.pipeline() as pipe:
+        for name, dictionary in content.items():
+            insert_to_redis(pipe, dictionary, name)
+
+        # Optionally, you can also create indexes for faster retrieval
+        # For example, if you frequently search users by email, you can create an index like this:
+        # for user in users:
+        #     pipe.set('email_index:' + user['email'], user['userID'])
+
+        pipe.execute()
+
+
+if '__name__' == '__main__':
+    main()
